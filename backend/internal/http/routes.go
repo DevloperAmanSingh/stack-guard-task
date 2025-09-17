1275377e99b4679eb8527bee82e3a293fbe92cd1
Ubuntu <ubuntu@ip-172-31-26-244.ec2.internal>
2025-09-17 14:09:01 +0000

    Update docker-compose and routes

--- backend/internal/http/routes.go
+++ backend/internal/http/routes.go
@@ -19,7 +19,7 @@ func SetupRoutes() *fiber.App {
 	})
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "*",
+		AllowOrigins:     "https://stack-guard-task.vercel.app",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders:     "*",
 		AllowCredentials: false,
